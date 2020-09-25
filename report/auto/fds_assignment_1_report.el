aea8e84a53047ee40931b364cb555377ded7303c
antonio mendes <17amendes@gmail.com>
2020-09-25 23:47:02 +0200

    Finished assignment!!!

--- report/auto/fds_assignment_1_report.el
+++ report/auto/fds_assignment_1_report.el
@@ -2,24 +2,25 @@
  "fds_assignment_1_report"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("hyperref" "hidelinks") ("datetime2" "useregional") ("biblatex" "backend=biber" "style=apa") ("geometry" "margin=1in" "left=1.5in" "includefoot")))
+                     '(("hyperref" "hidelinks") ("datetime2" "useregional") ("biblatex" "backend=biber" "style=apa") ("geometry" "margin=1in" "left=1in" "includefoot")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art10"
     "amsmath"
     "physics"
+    "wrapfig"
     "mathtools"
     "hyperref"
     "verbatim"
@@ -36,7 +37,12 @@
    (TeX-add-symbols
     '("email" 1)
     "HRule")
+   (LaTeX-add-labels
+    "fig:dense"
+    "fig:trump_polarity_02"
+    "fig:winner_states"
+    "fig:sentiment_states")
    (LaTeX-add-bibliographies
-    "cap_bibliography"))
+    "twitter_bibliography"))
  :latex)
 
